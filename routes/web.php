0a0eaf083223eda6d109b21c00edfcdadbc3d71e
velmouth-star <shanpuyangshu@gmail.com>
2022-09-21 11:37:16 +0900

    ルーティング作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+use App\Models\Customer;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('customers', CustomerController::class);
+Route::get('customers/search', [CustomerController::class, 'search']);
